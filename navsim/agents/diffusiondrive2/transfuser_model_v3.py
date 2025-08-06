ff11ade34dd3bad6f560a04c854ea5f521e2b549
Ftoy <spit10000@yahoo.gr>
2025-08-06 12:44:11 +0300

    ResNet18 gaze backbone and debug dims for keyval

--- navsim/agents/diffusiondrive2/transfuser_model_v3.py
+++ navsim/agents/diffusiondrive2/transfuser_model_v3.py
@@ -109,7 +109,8 @@ class V3TransfuserModel(nn.Module):
         bev_feature_upscale, bev_feature, _ = self._backbone(camera_feature, lidar_feature)
         print(f"Shape of gaze before processing {gaze_feature.shape}")
         gaze_feature_processed = self._gaze_backbone(gaze_feature)
-        # print(f"Shape of gaze after processing {gaze_feature_processed.shape}")
+        for feat in gaze_feature_processed:
+            print(f"Shape of gaze after processing {feat.shape}")
 
         cross_bev_feature = bev_feature_upscale
         bev_spatial_shape = bev_feature_upscale.shape[2:]
