5f0695d35f5d7b3d2c68a225a9e522aeab5975a9
Ftoy <spit10000@yahoo.gr>
2025-08-12 09:06:27 +0300

    Duplicate DD2 Agent to hidden to use for qformer

---
+++ navsim/agents/hidden/depth_gaze.py
@@ -0,0 +1,12 @@
+import torch  # need this import for transformers
+from transformers import pipeline
+
+print("Loading LiheYoung/depth-anything-small-hf")
+# load pipe
+pipe = pipeline(task="depth-estimation", model="LiheYoung/depth-anything-small-hf")
+
+
+def depth_inf(image):
+    # inference
+    depth = pipe(image)["depth"]
+    return depth
