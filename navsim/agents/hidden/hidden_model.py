0328e97b15f239ed1a9984cfb0bba7af7c64e21b
Ftoy <spit10000@yahoo.gr>
2025-08-13 17:14:25 +0300

    Revert

--- navsim/agents/hidden/hidden_model.py
+++ navsim/agents/hidden/hidden_model.py
@@ -125,7 +125,7 @@ class HiddenModel(nn.Module):
         # drop_prob = 0.15
         # if torch.rand(()) < drop_prob:
         #     print("Training without gaze")
-        #     gaze_feature = torch.zeros_like(gaze_feature)
+        gaze_feature = torch.zeros_like(gaze_feature)
         status_feature: torch.Tensor = features["status_feature"]
 
         batch_size = status_feature.shape[0]
