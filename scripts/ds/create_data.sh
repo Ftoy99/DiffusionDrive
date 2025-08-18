37b00f0d8f0703d375a211e74272ed0f975ac1ff
Ftoy <spit10000@yahoo.gr>
2025-08-18 16:04:43 +0300

    Removed the mmnet dependency hell by importing everything directly like a normal human being

--- scripts/ds/create_data.sh
+++ scripts/ds/create_data.sh
@@ -1,6 +1,6 @@
 python nuscenes_dir/nuscenes_converter.py nuscenes \
-    --root-path /mnt/ds/nuscenes_mini/ \
-    --canbus /mnt/ds/nuscenes_mini/ \
+    --root-path /mnt/ds/nuscenes_mini/v1.0-mini/ \
+    --canbus /mnt/ds/nuscenes_mini/v1.0-mini/ \
     --out-dir /mnt/ds/nuscenes_mini_processed \
     --extra-tag nuscenes \
     --version v1.0-mini
