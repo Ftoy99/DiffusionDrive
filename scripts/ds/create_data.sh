85e68ae29dc7723dfb12a3058d78f333e467b6c8
Ftoy <spit10000@yahoo.gr>
2025-08-18 12:34:16 +0300

    Nuscenes

--- scripts/ds/create_data.sh
+++ scripts/ds/create_data.sh
@@ -1,15 +1,6 @@
-export PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
-
 python nuscenes/nuscenes_converter.py nuscenes \
-    --root-path ./data/nuscenes \
-    --canbus ./data/nuscenes \
-    --out-dir ./data/infos/ \
+    --root-path /mnt/ds/nuscenes_mini/ \
+    --canbus /mnt/ds/nuscenes_mini/ \
+    --out-dir /mnt/ds/nuscenes_mini_processed \
     --extra-tag nuscenes \
     --version v1.0-mini
-
-python nuscenes/nuscenes_converter.py nuscenes \
-    --root-path ./data/nuscenes \
-    --canbus ./data/nuscenes \
-    --out-dir ./data/infos/ \
-    --extra-tag nuscenes \
-    --version v1.0
