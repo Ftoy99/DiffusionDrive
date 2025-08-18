c35393b82409d162c78ceb6fd08a8f3c87326601
Ftoy <spit10000@yahoo.gr>
2025-08-18 20:06:01 +0300

    Removed the mmnet dependency hell by importing everything directly like a normal human being

--- nuscenes_dir/nuscenes_converter.py
+++ nuscenes_dir/nuscenes_converter.py
@@ -414,19 +414,15 @@ def create_nuscenes_infos(root_path,
         data = dict(infos=train_nusc_infos, metadata=metadata)
         info_path = osp.join(out_path,
                              '{}_infos_train.pkl'.format(info_prefix))
-        # with open(info_path, 'wb') as f:
-        #     pickle.dump(data, f)
-        with open(info_path.replace("pkl","json"), "w") as f:
-            json.dump(data, f, indent=2)  # pretty print
+        with open(info_path, 'wb') as f:
+            pickle.dump(data, f)
             print(data)
         data['infos'] = val_nusc_infos
 
         info_val_path = osp.join(out_path,
                                  '{}_infos_val.pkl'.format(info_prefix))
-        # with open(info_val_path, 'wb') as f:
-        #     # pickle.dump(data, f)
-        with open(info_val_path.replace("pkl","json"), "w") as f:
-            json.dump(data, f, indent=2)  # pretty print
+        with open(info_val_path, 'wb') as f:
+            pickle.dump(data, f)
             print(data)
 
 
