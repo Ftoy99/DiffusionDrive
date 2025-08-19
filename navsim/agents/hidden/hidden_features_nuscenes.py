9d37dc6db70a8200d0b42f11dda1a9223147faf8
Ftoy <spit10000@yahoo.gr>
2025-08-19 14:03:06 +0300

    Nuscenes init files

--- navsim/agents/hidden/hidden_features_nuscenes.py
+++ navsim/agents/hidden/hidden_features_nuscenes.py
@@ -112,21 +112,22 @@ class HiddenFeatureBuilder(AbstractFeatureBuilder):
         # NOTE: Code from
         # https://github.com/autonomousvision/carla_garage/blob/main/team_code/data.py#L873
         def splat_points(point_cloud):
-            # 256 x 256 grid
-            xbins = np.linspace(
-                self._config.lidar_min_x,
-                self._config.lidar_max_x,
-                (self._config.lidar_max_x - self._config.lidar_min_x) * int(self._config.pixels_per_meter) + 1,
-            )
-            ybins = np.linspace(
-                self._config.lidar_min_y,
-                self._config.lidar_max_y,
-                (self._config.lidar_max_y - self._config.lidar_min_y) * int(self._config.pixels_per_meter) + 1,
-            )
-            hist = np.histogramdd(point_cloud[:, :2], bins=(xbins, ybins))[0]
-            hist[hist > self._config.hist_max_per_pixel] = self._config.hist_max_per_pixel
-            overhead_splat = hist / self._config.hist_max_per_pixel
-            return overhead_splat
+            # # 256 x 256 grid
+            # xbins = np.linspace(
+            #     self._config.lidar_min_x,
+            #     self._config.lidar_max_x,
+            #     (self._config.lidar_max_x - self._config.lidar_min_x) * int(self._config.pixels_per_meter) + 1,
+            # )
+            # ybins = np.linspace(
+            #     self._config.lidar_min_y,
+            #     self._config.lidar_max_y,
+            #     (self._config.lidar_max_y - self._config.lidar_min_y) * int(self._config.pixels_per_meter) + 1,
+            # )
+            # hist = np.histogramdd(point_cloud[:, :2], bins=(xbins, ybins))[0]
+            # hist[hist > self._config.hist_max_per_pixel] = self._config.hist_max_per_pixel
+            # overhead_splat = hist / self._config.hist_max_per_pixel
+            # return overhead_splat
+            return point_cloud
 
         # Remove points above the vehicle
         print("Before filter:", lidar_pc.shape)
