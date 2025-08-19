b96ef4d0a7be321e291b387950c7b104790d0761
Ftoy <spit10000@yahoo.gr>
2025-08-19 13:33:17 +0300

    Nuscenes init files

--- navsim/agents/hidden/hidden_features_nuscenes.py
+++ navsim/agents/hidden/hidden_features_nuscenes.py
@@ -28,6 +28,7 @@ from navsim.agents.hidden.depth_gaze import depth_inf
 
 front_cameras = ["CAM_FRONT", "CAM_FRONT_RIGHT", "CAM_FRONT_LEFT"]
 
+
 class NuFeatureData:
 
     def __init__(self):
@@ -107,26 +108,28 @@ class HiddenFeatureBuilder(AbstractFeatureBuilder):
 
         # only consider (x,y,z) & swap axes for (N,3) numpy array
         lidar_pc = agent_input.lidar[LidarIndex.POSITION].T
-        print(f"lidar_pc {lidar_pc}")
+        print(lidar_pc.shape)
+        print(lidar_pc[:5])  # show first 5 points
         # NOTE: Code from
         # https://github.com/autonomousvision/carla_garage/blob/main/team_code/data.py#L873
         def splat_points(point_cloud):
-            # # 256 x 256 grid
-            # xbins = np.linspace(
-            #     self._config.lidar_min_x,
-            #     self._config.lidar_max_x,
-            #     (self._config.lidar_max_x - self._config.lidar_min_x) * int(self._config.pixels_per_meter) + 1,
-            # )
-            # ybins = np.linspace(
-            #     self._config.lidar_min_y,
-            #     self._config.lidar_max_y,
-            #     (self._config.lidar_max_y - self._config.lidar_min_y) * int(self._config.pixels_per_meter) + 1,
-            # )
-            # hist = np.histogramdd(point_cloud[:, :2], bins=(xbins, ybins))[0]
-            # hist[hist > self._config.hist_max_per_pixel] = self._config.hist_max_per_pixel
-            # overhead_splat = hist / self._config.hist_max_per_pixel
-            # return overhead_splat
+            # 256 x 256 grid
+            xbins = np.linspace(
+                self._config.lidar_min_x,
+                self._config.lidar_max_x,
+                (self._config.lidar_max_x - self._config.lidar_min_x) * int(self._config.pixels_per_meter) + 1,
+            )
+            ybins = np.linspace(
+                self._config.lidar_min_y,
+                self._config.lidar_max_y,
+                (self._config.lidar_max_y - self._config.lidar_min_y) * int(self._config.pixels_per_meter) + 1,
+            )
+            hist = np.histogramdd(point_cloud[:, :2], bins=(xbins, ybins))[0]
+            hist[hist > self._config.hist_max_per_pixel] = self._config.hist_max_per_pixel
+            overhead_splat = hist / self._config.hist_max_per_pixel
+            return overhead_splat
             return point_cloud
+
         # Remove points above the vehicle
         lidar_pc = lidar_pc[lidar_pc[..., 2] < self._config.max_height_lidar]
         below = lidar_pc[lidar_pc[..., 2] <= self._config.lidar_split_height]
