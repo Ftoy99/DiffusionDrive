009ea30dc0bf43d0ec0278bfd494744d7f9aba6c
Ftoy <spit10000@yahoo.gr>
2025-10-12 16:56:04 +0300

    Adding mask for empty trajectories

---
+++ navsim/agents/hidden_v2/depth_gaze.py
@@ -0,0 +1,19 @@
+import torch  # need this import for transformers
+from transformers import pipeline, AutoImageProcessor
+
+print("Loading LiheYoung/depth-anything-small-hf")
+# load pipe
+processor = AutoImageProcessor.from_pretrained(
+    "LiheYoung/depth-anything-small-hf", use_fast=True
+)
+
+pipe = pipeline(
+    task="depth-estimation",
+    model="LiheYoung/depth-anything-small-hf",
+    image_processor=processor,
+)
+
+def depth_inf(image):
+    # inference
+    depth = pipe(image)["depth"]
+    return depth
